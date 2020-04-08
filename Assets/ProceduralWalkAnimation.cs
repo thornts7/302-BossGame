04606c7c04e9fe89876dc5b618270ab84446eb18
Shane Thornton <thornts7@ferris.edu>
2020-04-08 13:53:02 -0400

    More Additional Features

--- Assets/ProceduralWalkAnimation.cs
+++ Assets/ProceduralWalkAnimation.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 public class ProceduralWalkAnimation : MonoBehaviour
 {
     Vector3 startingPosition;
+    public Transform HintPos;
+    Vector3 hintStartingPos;
     float magnitude = 2;
+
+
+    public float sinWaveSpeed;
+    public float sinWaveOffset;
+
+    float feetDistancing = 1.5f;
+    float footHeight = .6f;
+    float howFarFeetGo = .5f;
+    PlayerController player;
+
     void Start()
     {
         startingPosition = transform.localPosition;
+        hintStartingPos = HintPos.localPosition;
+        player = GetComponentInParent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        float offsetZ = Mathf.Sin(Time.time)/magnitude;
-        float offestY = Mathf.Cos(Time.time)/magnitude;
-        if (offestY < 0) offestY = 0;
-
+        float time = (Time.time + sinWaveOffset * Mathf.PI) * sinWaveSpeed;
+        float offsetZ = Mathf.Cos(time)/magnitude;
+        float offestY = Mathf.Cos(time)/magnitude;
+        if (offestY < 0.1f) offestY = 0.1f;
+        
         Vector3 finalPosition = startingPosition;
+        Vector3 finalHintPosition = hintStartingPos;
 
-        finalPosition.y += offestY + .01f; // move final position up or down
-        finalPosition.z += offsetZ; // move forward and backward
+        finalPosition.x *= feetDistancing;
+        finalPosition.y += offestY * footHeight; // move final position up or down
+
+        Vector3 walkDir = transform.InverseTransformDirection(player.walkDir);
+        float Y = walkDir.y;
+        float Z = walkDir.z;
+        walkDir.z = Y;
+        walkDir.x = Z;
+        walkDir.y = 0;
+
+        finalPosition += walkDir * offsetZ * howFarFeetGo;
+        finalHintPosition += walkDir * offsetZ;
 
         transform.localPosition = finalPosition;
     }
