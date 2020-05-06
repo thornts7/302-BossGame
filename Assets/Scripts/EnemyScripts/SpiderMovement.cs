93e2bdbff6ac00ce2c7bb0ae300c037095b42d73
Shane Thornton <thornts7@ferris.edu>
2020-05-06 11:30:01 -0400

    New Stuff

---
+++ Assets/Scripts/EnemyScripts/SpiderMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderMovement : MonoBehaviour
+{
+    public List<GameObject> MovePoints = new List<GameObject>();
+    NavMeshAgent Enemy;
+    Vector3 StartPos;
+
+    void Start()
+    {
+        Enemy = gameObject.GetComponent<NavMeshAgent>();
+        Enemy.SetDestination(MovePoints[1].transform.position);
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, StartPos.y, transform.position.z);
+    }
+}
